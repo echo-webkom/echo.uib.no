fc430e5c44f24c72d7bdb7dc4f0771a39d1773a3
Bo Aanes <boaanes@gmail.com>
2021-05-07 16:12:09 +0200

    🚧 WIP on new member profile

--- src/components/member-profile.tsx
+++ src/components/member-profile.tsx
@@ -1,55 +1,56 @@
-import { Avatar, Box, Center, chakra, Text, useBreakpointValue } from '@chakra-ui/react';
+import { chakra, Flex, FlexProps, Text, VStack } from '@chakra-ui/react';
+import { randomColor, isDark } from '@chakra-ui/theme-tools';
 import Image from 'next/image';
 import React from 'react';
 import { Profile } from '../lib/api/student-group';
+import theme from '../styles/theme';
 
 const MemberImage = chakra(Image, {
-    baseStyle: { maxH: 128, maxW: 128 },
+    baseStyle: { borderRadius: '1em', maxH: 128, maxW: 128 },
     shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt', 'quality'].includes(prop),
 });
 
-const MemberProfile = ({ profile, role }: { profile: Profile; role: string }): JSX.Element => {
-    const memberImageSize = useBreakpointValue([96, 96, 128]);
+interface InitialProps extends FlexProps {
+    name: string;
+}
 
-    const getInitials = (name: string) => {
-        const words = name.split(' ');
-        return words.length >= 2 ? `${words[0].charAt(0)}${words[words.length - 1].charAt(0)}` : words[0].charAt(0);
-    };
+const InitialView = ({ name, ...rest }: InitialProps) => {
+    const words = name.split(' ');
+    const initials =
+        words.length >= 2 ? `${words[0].charAt(0)}${words[words.length - 1].charAt(0)}` : words[0].charAt(0);
+
+    const bg = randomColor({ string: name });
+    const color = isDark(bg)(theme) ? 'white' : 'gray.800';
 
     return (
-        <Box
-            p={['0', null, '.5em']}
-            w={['120px', null, '200px']}
-            overflow="visible"
-            textAlign="center"
-            fontSize={['.7em', null, '1em']}
-        >
-            <Center>
-                {profile.pictureUrl && (
-                    <MemberImage
-                        width={128}
-                        height={128}
-                        src={profile.pictureUrl}
-                        alt={profile.name}
-                        quality={100}
-                        w={memberImageSize}
-                        h={memberImageSize}
-                        borderRadius="100%"
-                    />
-                )}
-                {!profile.pictureUrl && (
-                    <Avatar
-                        getInitials={getInitials}
-                        size="2xl"
-                        name={profile.name}
-                        src={profile.pictureUrl || undefined}
-                        alt="bilde"
-                    />
-                )}
-            </Center>
-            <Text my=".5em">{profile.name}</Text>
-            <Text as="i">{role}</Text>
-        </Box>
+        <Flex bg={bg} borderRadius="1em" justifyContent="center" alignItems="center" {...rest}>
+            <Text color={color} fontSize="6em">
+                {initials}
+            </Text>
+        </Flex>
+    );
+};
+
+const MemberProfile = ({ profile, role }: { profile: Profile; role: string }): JSX.Element => {
+    return (
+        <VStack w={256} textAlign="center">
+            {profile.pictureUrl && (
+                <MemberImage
+                    src={profile.pictureUrl}
+                    alt={profile.name}
+                    quality={100}
+                    width={256}
+                    height={256}
+                    w={256}
+                    h={256}
+                />
+            )}
+            {!profile.pictureUrl && <InitialView name={profile.name} w={256} h={256} />}
+            <Text fontWeight="bold" fontSize="2xl">
+                {profile.name}
+            </Text>
+            <Text fontStyle="italic">{role}</Text>
+        </VStack>
     );
 };
 
